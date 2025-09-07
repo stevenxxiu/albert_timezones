1818648593afbb4400ab3ba3c46cb75331f16a39
Steven Xu <stevenxxiu@gmail.com>
2025-09-08 03:27:35 +1000

    chore: switch to *Basedpyright*

--- __init__.py
+++ __init__.py
@@ -1,15 +1,19 @@
 import json
 from datetime import datetime
 from pathlib import Path
+from typing import Callable, override
 
 import pytz
+from albert import setClipboardText  # pyright: ignore[reportUnknownVariableType]
 from albert import (
     Action,
     PluginInstance,
+    Query,
     StandardItem,
     TriggerQueryHandler,
-    setClipboardText,
 )
+
+setClipboardText: Callable[[str], None]
 
 md_iid = '3.0'
 md_version = '1.4'
@@ -23,6 +27,9 @@ md_lib_dependencies = ['pytz']
 ICON_URL = f'file:{Path(__file__).parent / "icons/datetime.png"}'
 
 
+TimezonesSettings = dict[str, str]
+
+
 class Plugin(PluginInstance, TriggerQueryHandler):
     def __init__(self):
         PluginInstance.__init__(self)
@@ -31,15 +38,17 @@ class Plugin(PluginInstance, TriggerQueryHandler):
         self.timezones: dict[str, pytz.tzinfo.BaseTzInfo] = {}
 
         with (self.configLocation() / 'settings.json').open() as sr:
-            settings = json.load(sr)
+            settings: TimezonesSettings = json.load(sr)  # pyright: ignore[reportAny]
             self.timezones = {
                 readable_name: pytz.timezone(timezone_name) for readable_name, timezone_name in settings.items()
             }
 
+    @override
     def defaultTrigger(self):
         return 'tz '
 
-    def handleTriggerQuery(self, query) -> None:
+    @override
+    def handleTriggerQuery(self, query: Query) -> None:
         cur_time = datetime.now()
         fmt = '%Y/%m/%d %-I:%M:%S %p %z'
 
@@ -47,14 +56,13 @@ class Plugin(PluginInstance, TriggerQueryHandler):
             dest_time_str = cur_time.astimezone(timezone).strftime(fmt).lower()
             copyable = f'{readable_name}: {dest_time_str}'
 
-            query.add(
+            copy_call: Callable[[str], None] = lambda value_=copyable: setClipboardText(value_)  # noqa: E731
+            query.add(  # pyright: ignore[reportUnknownMemberType]
                 StandardItem(
                     id=f'{md_name}/{readable_name}',
                     text=dest_time_str,
                     subtext=readable_name,
                     iconUrls=[ICON_URL],
-                    actions=[
-                        Action(f'{md_name}/{readable_name}', 'Copy', lambda value_=copyable: setClipboardText(value_))
-                    ],
+                    actions=[Action(f'{md_name}/{readable_name}', 'Copy', copy_call)],
                 )
             )
